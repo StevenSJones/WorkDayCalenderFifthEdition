078ac275c25041c85350e544c1b1d34410764266
SS Jones <SSJones.code@gmail.com>
2020-08-14 11:25:14 -0700

    Got the page in working order again

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-// 1 - instantiate/invoke a moment object
-let nowMoment = moment();
-  
-// instantiate a JavaScript Date object
-var NowDate = new Date();
+// 1 created a var and set it equal to the new Date () method that we get back from moment.js
+let responseNowDate = new Date();
+// console.log(new Date().getHours());
 
+//If the newDate response from the get hours method is less than 12 then say good morning
+if (new Date().getHours() < 12) {
+  $("#demo").text("Good Morning!");//Does the same as the line beneath that is commented out 
+  //   document.getElementById("demo").innerHTML = "Good Afternoon!";
+} else {//else, the newDate response from the get hours method is more than 12 and you see good evenin.
+  $("demo").text("Good Evening!"); //Does the same as the line beneath thatis commented out
+  //   document.getElementById("demo").innerHTML = "Good Afternoon!";
+}
 
-
-//this will grab moment obj
-$("#currentDay").text(nowMoment.format('dddd, MMMM Do YYYY, h:mm:ss a'));
-
-
-
-
-
-
-
-// var NowMoment = moment();
-// var eDisplayMoment = document.getElementById('displayMoment');
-// eDisplayMoment.innerHTML = NowMoment.format('YYYY-M-D');
+//2 this will grab moment obj
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+//4
+$(".saveBtn").on("click", function (e) {
+  e.preventDefault();
+  const txt = $(this).parent().siblings(".description").children().val();
+  console.log(txt);
+});
